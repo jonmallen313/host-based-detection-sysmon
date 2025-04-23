711cb15298b5a71380216fc95e35a42c4ac4f318
jonmallen313 <98624797+jonmallen313@users.noreply.github.com>
2025-04-23 14:13:42 -0700

    Update pipe_server.ps1

--- scripts/pipe_server.ps1
+++ scripts/pipe_server.ps1
@@ -1,1 +1,4 @@
-
+Start-Sleep -Seconds 2 #wait a bit so server starts first
+$pipeName = 'mypipe123'
+$pipeClient = New-object System. IO. Pipes. NamedPipeClientStream('.', $pipeName,
+[System. IO. Pipes. PipeDirection] : : Out)
