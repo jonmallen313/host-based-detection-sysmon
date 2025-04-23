cd9ecaa510574511118d5e254129a5110cb05ed6
jonmallen313 <98624797+jonmallen313@users.noreply.github.com>
2025-04-23 14:14:08 -0700

    Update pipe_client.ps1

--- scripts/pipe_client.ps1
+++ scripts/pipe_client.ps1
@@ -1,1 +1,5 @@
-
+$pipeName = 'mypipe123'
+$pipeServer = New-Object System. IO. Pipes. NamedPipeServerStream($pipeName)
+Write-Host "Waiting for connection on pipe: $pipeName ... "
+$pipeServer.WaitForConnection()
+Write-Host "Client connected!"
